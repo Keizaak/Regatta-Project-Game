d509d33ec373642e5293ed5ffde1fc13c1318a48
Camille GUIGNOL <camille.guignol@ecole.ensicaen.fr>
2020-10-22 10:17:23 +0200

    Cap + vecteur de direction + cap => vecteur

--- src/main/java/fr/ensicaen/genielogiciel/mvp/model/boat/Regalata.java
+++ src/main/java/fr/ensicaen/genielogiciel/mvp/model/boat/Regalata.java
@@ -1,4 +1,27 @@
 package fr.ensicaen.genielogiciel.mvp.model.boat;
 
+import java.util.Vector;
+
 public class Regalata extends Boat {
+
+    Vector getDirection(Cap cap) {
+        switch (cap) {
+            case NORTH:
+                return new Vector(0, 1);
+            case EAST:
+                return new Vector(1, 0);
+            case SOUTH:
+                return new Vector(0, -1);
+            case WEST:
+                return new Vector(-1, 0);
+            default:
+                return new Vector();
+        }
+    }
+
+    void changeDirection(Vector v){
+        /* position * direction */
+    }
+
+
 }
