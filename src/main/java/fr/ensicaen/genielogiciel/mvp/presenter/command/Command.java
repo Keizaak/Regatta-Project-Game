798ac434522928b547cefe468b957917e4aceeb4
jpoullain <jeremy.poullain@ecole.ensicaen.fr>
2020-11-04 10:30:16 +0100

    Changement constructeur Command

--- src/main/java/fr/ensicaen/genielogiciel/mvp/presenter/command/Command.java
+++ src/main/java/fr/ensicaen/genielogiciel/mvp/presenter/command/Command.java
@@ -1,6 +1,14 @@
 package fr.ensicaen.genielogiciel.mvp.presenter.command;
 
+import fr.ensicaen.genielogiciel.mvp.model.Model;
+
 public abstract class Command {
+    private Model _model;
+
+    public Command(Model model) {
+        _model = model;
+    }
+
     public abstract void execute();
     public abstract void undo();
 }
