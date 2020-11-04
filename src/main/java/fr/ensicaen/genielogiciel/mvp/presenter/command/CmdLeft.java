ae029679d016f677ce78bb0ba87876757f4887f6
jpoullain <jeremy.poullain@ecole.ensicaen.fr>
2020-11-04 10:32:29 +0100

    Changement constructeur CmdLeft et CmdRight

--- src/main/java/fr/ensicaen/genielogiciel/mvp/presenter/command/CmdLeft.java
+++ src/main/java/fr/ensicaen/genielogiciel/mvp/presenter/command/CmdLeft.java
@@ -1,6 +1,12 @@
 package fr.ensicaen.genielogiciel.mvp.presenter.command;
 
+import fr.ensicaen.genielogiciel.mvp.model.Model;
+
 public class CmdLeft extends Command {
+
+    public CmdLeft(Model model) {
+        super(model);
+    }
 
     @Override
     public void execute() {
--- src/main/java/fr/ensicaen/genielogiciel/mvp/presenter/command/CmdRight.java
+++ src/main/java/fr/ensicaen/genielogiciel/mvp/presenter/command/CmdRight.java
@@ -1,6 +1,12 @@
 package fr.ensicaen.genielogiciel.mvp.presenter.command;
 
+import fr.ensicaen.genielogiciel.mvp.model.Model;
+
 public class CmdRight extends Command {
+
+    public CmdRight(Model model) {
+        super(model);
+    }
 
     @Override
     public void execute() {
