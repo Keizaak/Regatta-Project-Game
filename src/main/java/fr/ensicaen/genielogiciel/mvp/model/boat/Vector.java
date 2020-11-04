ece3e3e24f8d55a16b991d82322f7496aae118e8
Keizaak <37865090+Keizaak@users.noreply.github.com>
2020-11-04 10:33:31 +0100

    - Functions to change the boat position and orientation - Tests

--- src/main/java/fr/ensicaen/genielogiciel/mvp/model/boat/Vector.java
+++ src/main/java/fr/ensicaen/genielogiciel/mvp/model/boat/Vector.java
@@ -1,17 +1,32 @@
 package fr.ensicaen.genielogiciel.mvp.model.boat;
 
+import java.util.Objects;
+
 public class Vector {
-    public int _x;
-    public int _y;
+    public float _x;
+    public float _y;
 
     public Vector() {
         _x = 0;
         _y = 0;
     }
 
-    public Vector(int x, int y) {
+    public Vector(float x, float y) {
         _x = x;
         _y = y;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vector vector = (Vector) o;
+        return Float.compare(vector._x, _x) == 0 &&
+                Float.compare(vector._y, _y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(_x, _y);
+    }
 }
---
+++ src/test/java/fr/ensicaen/genielogiciel/mvp/model/boat/RegalataTest.java
@@ -0,0 +1,31 @@
+package fr.ensicaen.genielogiciel.mvp.model.boat;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RegalataTest {
+
+    @Test
+    public void getDirectionTest() {
+        Regalata regalata = new Regalata();
+
+        Boat.Cap testCap = Boat.Cap.EAST;
+        assertEquals(new Vector(1, 0), regalata.getDirection(testCap));
+
+        testCap = Boat.Cap.WEST;
+        assertEquals(new Vector(-1, 0), regalata.getDirection(testCap));
+    }
+
+    /* TODO: Test changeOrientation */
+
+    @Test
+    public void changePositionTest() {
+        Regalata regalata = new Regalata();
+
+        Vector directionTest = new Vector(-1, 0);
+        Vector positionTest = new Vector(1, 1);
+
+        assertEquals(new Vector(0, 1), regalata.changePosition(directionTest, positionTest));
+    }
+}
