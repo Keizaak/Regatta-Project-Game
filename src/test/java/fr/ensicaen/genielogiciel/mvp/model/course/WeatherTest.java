74a4d008a32903ce6b69925a5611a4f43ce71215
Genevieve Masioni <masionigenevieve@gmail.com>
2020-11-27 10:15:44 +0100

    Split and refactor Weather class

--- src/test/java/fr/ensicaen/genielogiciel/mvp/model/course/WeatherTest.java
+++ src/test/java/fr/ensicaen/genielogiciel/mvp/model/course/WeatherTest.java
@@ -10,23 +10,20 @@ import static org.junit.Assert.*;
 public class WeatherTest {
     private String _latitude = "49.283";
     private String _longitude = "-0.25";
+    private String _url = "https://www.prevision-meteo.ch/services/json/lat=" + _latitude + "lng=" + _longitude;
 
     @Test
     public void load_weather_info() {
-        // Not testable
-    }
-
-    @Test
-    public void set_wind_info() {
         Weather weather = new Weather(_latitude, _longitude);
+        WeatherLoader weatherLoader = new WeatherLoader(_url);
         String json_data;
         float wind_speed;
         String wind_direction;
         try {
-            json_data = weather.load_weather_info();
+            json_data = weatherLoader.load_weather_info();
             wind_speed = weather.get_wind_speed();
             wind_direction = weather.get_wind_direction();
-            weather.set_wind_info(json_data);
+            weather.load_wind_info(json_data);
             assertNotEquals(null, wind_speed, weather.get_wind_speed());
             assertNotEquals(null, wind_direction, weather.get_wind_direction());
         } catch (IOException e) {
