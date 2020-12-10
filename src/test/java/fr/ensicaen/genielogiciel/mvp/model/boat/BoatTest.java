6913d50bbd70215f458a34eb05ada065f99846d5
Camille GUIGNOL <camille.guignol@ecole.ensicaen.fr>
2020-12-10 11:08:45 +0100

    Added tests of the class boat

--- src/test/java/fr/ensicaen/genielogiciel/mvp/model/boat/BoatTest.java
+++ src/test/java/fr/ensicaen/genielogiciel/mvp/model/boat/BoatTest.java
@@ -3,22 +3,58 @@ package fr.ensicaen.genielogiciel.mvp.model.boat;
 import fr.ensicaen.genielogiciel.mvp.model.Vector;
 import org.junit.Test;
 
-import java.lang.reflect.Method;
-
 import static org.junit.Assert.assertEquals;
 
 public class BoatTest {
 
     @Test
-    public void getDirectionTest() {
-        Regalata regalata = new Regalata(Cap.SOUTH, 0.80f);
+    public void getVectorDirectionTest() {
+        Regalata regalata = new Regalata(Cap.SOUTH, 80f);
 
         Cap testCap = Cap.EAST;
-        assertEquals(new Vector(1, 0), regalata.getDirection(testCap));
+        assertEquals(new Vector(1, 0), regalata.getVectorDirection(testCap));
 
         testCap = Cap.WEST;
-        assertEquals(new Vector(-1, 0), regalata.getDirection(testCap));
+        assertEquals(new Vector(-1, 0), regalata.getVectorDirection(testCap));
     }
 
-    /* TODO: mettre les méthodes getter attributs à tester en package visibility */
+    @Test
+    public void changeDirectionTest() {
+        Regalata regalata = new Regalata(Cap.SOUTH, 80f);
+
+        regalata.setOrientation(320f);
+        regalata.changeDirection();
+        assertEquals(Cap.EAST, regalata.getDirection());
+
+        regalata.setOrientation(50f);
+        regalata.changeDirection();
+        assertEquals(Cap.SOUTH, regalata.getDirection());
+
+        regalata.setOrientation(270f);
+        regalata.changeDirection();
+        assertEquals(Cap.NORTH, regalata.getDirection());
+
+        regalata.setOrientation(150f);
+        regalata.changeDirection();
+        assertEquals(Cap.WEST, regalata.getDirection());
+    }
+
+    @Test
+    public void addValueToOrientationTest() {
+        Regalata regalata = new Regalata(Cap.SOUTH, 80f);
+        regalata.addValueToOrientation(1f);
+        assertEquals(271f, regalata.getOrientation(), 0.9);
+    }
+
+    @Test
+    public void changePositionTest() {
+        Regalata regalata = new Regalata(Cap.SOUTH, 80f);
+
+        regalata.changePosition();
+
+        System.out.println(regalata.getPosition().x + " " + regalata.getPosition().y);
+
+        assertEquals(0f, regalata.getPosition().x, 0.000000000001f);
+        assertEquals(-1.6f, regalata.getPosition().y, 0.1f);
+    }
 }
