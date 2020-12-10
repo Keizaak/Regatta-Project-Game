f4766a2d5d235ae371560f0199ca5f2bf9d05fdb
nathan <nathan.michel@ecole.ensicaen.fr>
2020-12-10 10:04:44 +0100

    Buoy update isValidated

--- src/main/java/fr/ensicaen/genielogiciel/mvp/model/course/Buoy.java
+++ src/main/java/fr/ensicaen/genielogiciel/mvp/model/course/Buoy.java
@@ -1,17 +1,22 @@
 package fr.ensicaen.genielogiciel.mvp.model.course;
 
 import fr.ensicaen.genielogiciel.mvp.model.Vector;
+import fr.ensicaen.genielogiciel.mvp.model.boat.Boat;
 
 import java.util.Observable;
 import java.util.Observer;
 
+import static java.lang.Math.abs;
+
 public class Buoy implements Observer {
     private final Vector _position;
     private final Vector _limit;
+    private boolean _isValidated;
 
     public Buoy(Vector position, Vector limit) {
         _position = position;
         _limit = limit;
+        _isValidated = false;
     }
 
     public Vector getPosition() {
@@ -20,6 +25,23 @@ public class Buoy implements Observer {
 
     @Override
     public void update(Observable o, Object arg) {
-
+        Boat regatta = ((Boat)o);
+        Vector v = regatta.getPosition();
+        if (_position.y == _limit.y) {
+            if ((v.x > _position.x && v.x < _limit.x) || (v.x < _position.x && v.x > _limit.x)) {
+                if (abs(v.y - _position.y) < 10) {
+                    _isValidated = true;
+                }
+            }
+        }
+        else {
+            if ((v.y > _position.y && v.y < _limit.y) || (v.y < _position.y && v.y > _limit.y)) {
+                if (abs(v.x - _position.x) < 10) {
+                    _isValidated = true;
+                }
+            }
+        }
     }
 }
+
+
